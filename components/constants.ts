32037b3bb134366d21987a9872911a607740f316
thegamerhat <thegamerhat@cybrdise.com>
2023-02-03 13:25:02 +0000

    fix api link

--- components/constants.ts
+++ components/constants.ts
@@ -1,5 +1,5 @@
-export const ProjectURL = "http://localhost:3001";
-// export const ProjectURL = "https://fireship-remotion-intro.vercel.app/";
+// export const ProjectURL = "http://localhost:3000";
+export const ProjectURL = "https://fireship-remotion-intro.vercel.app/";
 
 export const animationVideoURL =
   "https://res.cloudinary.com/drnm9lhef/video/upload/v1675337644/fireship-remotion-intro/CRAnimation.mp4";
