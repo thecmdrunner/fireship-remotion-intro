4ec4add33af46ce3991d1729e3e962fa56e74dea
thegamerhat <thegamerhat@cybrdise.com>
2023-02-06 10:16:43 +0000

    changed URL

--- components/constants.ts
+++ components/constants.ts
@@ -2,4 +2,4 @@
 export const ProjectURL = "https://fireship-remotion-intro.vercel.app/";
 
 export const animationVideoURL =
-  "https://res.cloudinary.com/drnm9lhef/video/upload/v1675337644/fireship-remotion-intro/CRAnimation.mp4";
+  "https://res.cloudinary.com/drnm9lhef/video/upload/v1675678500/fireship-remotion-intro/CRAnimation.mp4";
