c72580dc6ff71e44fe62683346bcda5aaabe3840
thecmdrunner <thecmdrunner@proton.me>
2023-08-15 15:43:03 +0530

    add urls

--- components/constants.ts
+++ components/constants.ts
@@ -1,8 +1,12 @@
-// export const ProjectURL = "http://localhost:3000";
-export const ProjectURL = "https://fireship-remotion-intro.vercel.app/";
+export const backends = {
+  lambdaVercel: "https://fireship-remotion-intro.vercel.app/",
+  localhost: "http://localhost:3000",
+};
+
+export const ProjectURL = backends.localhost;
 
 // Toggle this to allow/restrict new audio generation.
-export const API_ACCESS_ALLOWED = false;
+export const API_ACCESS_ALLOWED = true;
 
 export const animationVideoURL =
   "https://res.cloudinary.com/drnm9lhef/video/upload/v1675678500/fireship-remotion-intro/CRAnimation.mp4";
